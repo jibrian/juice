e50558e7427849d5beaf322c8b66c930a2edc0eb
Brian Ji <brian@restorationmedia.com>
2015-07-06 17:31:34 -0700

    improve redirect

--- public/redirect-trace.php
+++ public/redirect-trace.php
@@ -4,8 +4,11 @@ $url = $_GET["url"];
 $headers = get_headers($url, 1);
 $location = $headers["Location"];
 
-if ($location) {
+if (gettype($location) === "array") {
 	echo json_encode($location);
+} else if (gettype($location) === "string") {
+	$output = [$location];
+	echo json_encode($output);
 } else {
 	$output = ["End of the breadcrumbs trail"];
 	echo json_encode($output);
