8516b870396838e7ea94118f6f502f4cab77abc0
Brian Ji <jibrianh@gmail.com>
2015-07-12 16:10:25 -0700

    fix type in json juxto alg

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -452,7 +452,7 @@ module.exports = ItemViewPrototype.extend({
 		var $keys = this.$el.find(".key");
 		var $vals = this.$el.find(".value");
 		$keys.each(function(index, item) {
-			if (options.json1[item.innerHTML] === "" && options.json1[item.innerHTML] ==="") {
+			if (options.json1[item.innerHTML] === "" && options.json2[item.innerHTML] ==="") {
 				$(item).addClass("match");
 				$vals.eq(index).addClass("match");
 			} else if (options.json1[item.innerHTML] && options.json2[item.innerHTML]) {
